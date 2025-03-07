dc8263113a048a8a8e938f42ab94742e12871661
Aryaman2063 <150682745+Aryaman2063@users.noreply.github.com>
2025-03-07 12:13:56 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE userdb;
+
+USE userdb;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
